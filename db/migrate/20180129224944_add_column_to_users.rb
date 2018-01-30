51ab81f6609817dbdea05c5a64c314418c19c53d
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-30 13:35:12 -0700

    add user model, create dashboard inddex views, add db migrates for user and updated schema

---
+++ db/migrate/20180129224944_add_column_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :location, :string
+    add_column :users, :bio, :string
+    add_column :users, :name, :string
+  end
+end
