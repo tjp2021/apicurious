60da6ee739d24c5f431d9b8fa8c62ac4c4a7f312
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-30 15:59:05 -0700

    reset database and rerun migrations

---
+++ db/migrate/20180130222550_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :nickname
+      t.string :name
+      t.string :token
+
+      t.timestamps
+    end
+  end
+end
