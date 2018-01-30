84cc49235a58490c24edb5374922c65440586009
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-30 15:24:22 -0700

    Revise feature spec, specifically stub method, revise ssions controller

---
+++ spec/features/user_logs_in_with_github_spec.rb
@@ -0,0 +1,26 @@
+describe "user logs in" do
+  scenario "using github oauth" do
+    stub_omniauth
+    visit root_path
+    expect(page).to have_link("Sign in with Github")
+    click_link "Sign in with Github"
+    expect(page).to have_content("Timothy Joo")
+    expect(page).to have_link("Logout")
+  end
+end
+
+  def stub_omniauth
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      provider: "github",
+      uid: "12345678910",
+      info: {
+        nickname: "tyjoo26",
+        name: "Timothy Joo"
+      },
+      credentials: {
+        token: "abcdefg12345",
+        expires: false,
+      }
+    })
+  end
