5ac7daa7354658eb903909bdff5e1ef2f15dfc6b
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-30 15:58:25 -0700

    Build out create method with update_or_create method, redirect needs to be fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
   def create
-    
+    user = User.update_or_create(request.env["omniauth.auth"])
+    session[:id] = user.id
+
+    redirect_to root_path  
   end
 end
