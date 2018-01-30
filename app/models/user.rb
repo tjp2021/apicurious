95090ddd76678b9105eb3f7180249c97cee2d73e
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-30 15:58:39 -0700

    Create update_or_create method in user model

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+
+  def self.update_or_create(auth)
+    user = User.find_by(uid: auth[:uid]) || User.new
+    user.attributes = {
+                      provider: auth[:provider],
+                      uid: auth[:uid],
+                      nickname: auth[:info][:nickname],
+                      name: auth[:info][:name],
+                      token: auth[:credentials][:token]
+                      }
+
+    user.save!
+    user
+  end
+end
