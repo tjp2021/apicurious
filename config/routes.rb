0b176eaab2f613c40f1d1b029a6279c2bb96a12d
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-02 09:38:48 -0700

    clean up route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-<<<<<<< HEAD
-  get '/auth/github/callback', to: "sessions#create"
-=======
+
   get '/auth/github/callback', to: 'sessions#create'
->>>>>>> master
+
 
   resources :dashboard, only: [:index]
 end
